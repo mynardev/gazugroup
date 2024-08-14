d67f4dd0c22808bf252e262165439bd75db93c2b
mynardev <mynar.dev@gmail.com>
2024-08-14 21:00:58 +0200

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // Adjust the path based on your project structure
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
